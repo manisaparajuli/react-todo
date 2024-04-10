d91129446131271fffe1140c7ab633b473311cae
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-10 13:34:48 +1000

    input box and button added to app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; 
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addNewTask = () => {
+    const newTodoList = [...todoList, newTask]
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addNewTask">
+        <input type="text" onChange={handleChange}/>
+        <button onClick={addNewTask}>Add new task</button>
+      </div>
+      <div className="taskList">
+      </div>
     </div>
   );
 }
