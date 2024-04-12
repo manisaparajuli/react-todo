7171ba044f3258a480f3b9f478f6d91dcab4056e
manisaparajuli <manisaparajuli66@gmail.com>
2024-04-12 15:40:24 +1000

    save task button added

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,17 +5,16 @@ import { Task } from './Task';
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
-  const [edit, setEdit] = useState(false)
 
   const handleChange = (event) => {
     setNewTask(event.target.value);
   }
-
   const addNewTask = () => {
     const task = {
       id: todoList.length ? todoList[todoList.length -1].id + 1 : 0 ,
       taskName: newTask,
-      completed: false
+      completed: false,
+      edit: false
     }
     setTodoList([...todoList, task])
   }
@@ -37,8 +36,34 @@ function App() {
     }))
   }
 
-  const editTask = () => {
-    setEdit(true)
+  const editTask = (id) => {
+    setTodoList(todoList.map((task) => {
+      if (task.id === id ){
+        return {...task, edit: true}
+      }else{
+        return task;
+      }
+    }))
+  }
+
+  const setUpdate = (id, updateTask) => {
+    setTodoList( todoList.map((task) =>{
+      if (task.id === id ){
+        return task.taskName = updateTask
+      }else{
+        return task;
+      }
+    }))
+  };
+
+  const saveTask = (id) => {
+    setNewTask( todoList.map((task) =>{
+      if (task.id === id ){
+        return {...task, edit: false}
+      }else{
+        return task;
+      }
+    }))
   }
 
   return (
@@ -49,14 +74,19 @@ function App() {
       </div>
       <div className="taskList">
         {todoList.map((task)=> {
-          return (
-            <Task  id={task.id} 
-            taskName={task.taskName} 
-            deleteTask={deleteTask} 
-            toggleComplete={toggleComplete} 
-            completed={task.completed}
-            edit={editTask}
-            />
+          return ( <div key={task.id}>
+              
+              <Task  id={task.id} 
+              taskName={task.taskName} 
+              deleteTask={deleteTask} 
+              toggleComplete={toggleComplete} 
+              completed={task.completed}
+              editTask={editTask}
+              edit={task.edit}
+              setUpdate={setUpdate}
+              saveTask={saveTask}
+              />
+            </div>
           )
         })}
       </div>
--- todo/src/Task.js
+++ todo/src/Task.js
@@ -1,11 +1,14 @@
 export const Task = (props) =>{
   return (
-    <div className="task" style={{backgroundColor: props.completed ? "green": "white"}}>
-      <h2>{props.taskName}</h2> 
+    <div  className="task" style={{backgroundColor: props.completed ? "green": "white"}}>
+      <h2 style={{display: props.edit ? "none" : "inline" }}>{props.taskName}</h2> 
+      <input style={{display: props.edit ? "inline" : "none"}} type="text" defaultValue={props.taskName}
+      onChange={() => props.setUpdate(props.taskName, props.id)} />
       <button onClick={() => props.toggleComplete(props.id)}>Complete</button>
       <button onClick={() => props.deleteTask(props.id)}>Delete</button>
-      <button onClick={props.editTask}>Edit</button>
-      <input type="text" value={props.taskName}  />
+      <button onClick={() => props.editTask(props.id)}>Edit</button>
+      <button onClick={() => props.saveTask(props.id)}>Save</button>
+      
     </div>
   )
 }
